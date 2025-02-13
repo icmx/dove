b1f4c9afcd83fb8a4e2643436c0d2cd5e339afc7
icmx <32983315+icmx@users.noreply.github.com>
2025-02-13 21:38:41 +0600

    implement db

---
+++ src/core/db.ts
@@ -0,0 +1,216 @@
+import { JSONFileSyncPreset } from 'lowdb/node';
+import { Env } from '../shared/env';
+
+export namespace DB {
+  export type Collection<TEntry> = {
+    counter: number;
+    entries: TEntry[];
+  };
+
+  export type Ban = {
+    created: number;
+    expires: number;
+  };
+
+  export type Restriction = 'warning' | 'ban';
+
+  export type Reply = {
+    id: number;
+    created: number;
+    content: string;
+    restriction?: Restriction;
+    ipHash: string;
+    passwordHash: string;
+  };
+
+  export interface Thread {
+    id: number;
+    created: number;
+    locked?: boolean;
+    content: string;
+    restriction?: Restriction;
+    ipHash: string;
+    passwordHash?: string;
+    replies: Collection<Reply>;
+  }
+
+  export const db = JSONFileSyncPreset<{
+    bans: Record<string, Ban | undefined>;
+    threads: Collection<Thread>;
+  }>(Env.config.DOVE_DB_FILENAME, {
+    bans: {},
+    threads: {
+      counter: 0,
+      entries: [],
+    },
+  });
+
+  export const timestamp = (): number => {
+    return Math.round(Date.now() / 1000);
+  };
+
+  export const mutate = <TResult>(mutation: () => TResult): TResult => {
+    db.read();
+
+    const result = mutation();
+
+    db.write();
+
+    return result;
+  };
+
+  export const insertBan = (
+    ipHash: string,
+    data: Pick<Ban, 'expires'>
+  ): void => {
+    const created = timestamp();
+
+    db.data.bans[ipHash] = { created, ...data };
+  };
+
+  export const selectBan = (ipHash: string): Ban | null => {
+    const ban = db.data.bans[ipHash] || null;
+
+    return ban;
+  };
+
+  export const deleteBans = (ipHashes: string[]): void => {
+    ipHashes.forEach((ipHash) => {
+      db.data.bans[ipHash] = undefined;
+    });
+  };
+
+  export const insertThread = (
+    data: Pick<Thread, 'content' | 'ipHash' | 'passwordHash'>
+  ): number => {
+    const id = db.data.threads.counter + 1;
+    const created = timestamp();
+    const replies: Collection<Reply> = {
+      counter: 0,
+      entries: [],
+    };
+
+    const thread: Thread = {
+      id,
+      created,
+      ...data,
+      replies,
+    };
+
+    db.data.threads.entries.push(thread);
+    db.data.threads.counter = id;
+
+    return id;
+  };
+
+  export const selectThreads = (): Thread[] => {
+    return db.data.threads.entries;
+  };
+
+  export const selectThreadsByIds = (ids: number[]): Thread[] => {
+    return selectThreads().filter((thread) => ids.includes(thread.id));
+  };
+
+  export const selectThreadById = (id: number): Thread => {
+    const thread = db.data.threads.entries.find(
+      (entry) => entry.id === id
+    );
+
+    if (!thread) {
+      throw new Error(`Thread @${id} not found`);
+    }
+
+    return thread;
+  };
+
+  export const updateThreads = (
+    ids: number[],
+    data: Pick<Thread, 'restriction' | 'locked'>
+  ): void => {
+    db.data.threads.entries = db.data.threads.entries.map((entry) => {
+      if (!ids.includes(entry.id)) {
+        return entry;
+      }
+
+      return { ...entry, ...data };
+    });
+  };
+
+  export const deleteThreads = (ids: number[]): void => {
+    db.data.threads.entries = db.data.threads.entries.filter(
+      (entry) => {
+        return !ids.includes(entry.id);
+      }
+    );
+  };
+
+  export const insertReply = (
+    threadId: number,
+    data: Pick<Reply, 'content' | 'ipHash' | 'passwordHash'>
+  ): number => {
+    const thread = selectThreadById(threadId);
+
+    const id = thread.replies.counter + 1;
+    const created = timestamp();
+
+    const reply: Reply = {
+      id,
+      created,
+      ...data,
+    };
+
+    thread.replies.entries.push(reply);
+    thread.replies.counter = id;
+
+    return id;
+  };
+
+  export const selectReplies = (threadId: number): Reply[] => {
+    const thread = selectThreadById(threadId);
+
+    return thread.replies.entries;
+  };
+
+  export const selectReplyById = (
+    threadId: number,
+    id: number
+  ): Reply => {
+    const thread = selectThreadById(threadId);
+    const reply = thread.replies.entries.find(
+      (entry) => entry.id === id
+    );
+
+    if (!reply) {
+      throw new Error(`Reply #${id} in thread @${threadId} not found`);
+    }
+
+    return reply;
+  };
+
+  export const updateReplies = (
+    threadId: number,
+    ids: number[],
+    data: Pick<Reply, 'restriction'>
+  ): void => {
+    const thread = selectThreadById(threadId);
+
+    thread.replies.entries = thread.replies.entries.map((entry) => {
+      if (!ids.includes(entry.id)) {
+        return entry;
+      }
+
+      return { ...entry, ...data };
+    });
+  };
+
+  export const deleteReplies = (
+    threadId: number,
+    ids: number[]
+  ): void => {
+    const thread = selectThreadById(threadId);
+
+    thread.replies.entries = thread.replies.entries.filter((entry) => {
+      return !ids.includes(entry.id);
+    });
+  };
+}
---
+++ src/shared/env.ts
@@ -0,0 +1,17 @@
+import z from 'zod';
+
+export namespace Env {
+  const schema = z.object({
+    DOVE_HOST: z.string().default('[::1]'),
+    DOVE_PORT: z.number({ coerce: true }).int().default(3000),
+    DOVE_DB_FILENAME: z.string().default('./db.dev.json'),
+  });
+
+  const values = Object.fromEntries(
+    Object.entries(schema.shape).map(([key]) => {
+      return [key, process.env[key]];
+    })
+  );
+
+  export const config = schema.parse(values);
+}
